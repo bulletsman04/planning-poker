6b62b9ae3b81afd282f93fd6a8d6cabca6706e3e
mwielgosz04 <mwielgosz04@gmail.com>
2020-03-27 15:46:43 +0100

    added basic routing

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 
 import { Routes, RouterModule } from '@angular/router';
+import { StartScreenComponent } from './start-screen/start-screen.component';
+import { CreateSessionComponent } from './create-session/create-session.component';
+import { JoinScreenComponent } from './join-screen/join-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/start', pathMatch: 'full' },
+  { path: 'start', component: StartScreenComponent},
+  { path: 'create', component: CreateSessionComponent},
+  { path: 'join', component: JoinScreenComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
