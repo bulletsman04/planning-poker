6b42f8ca2b4ff348fb4bfb504b8c0d807604111c
mwielgosz04 <mwielgosz04@gmail.com>
2020-03-27 16:58:25 +0100

    added signal service

---
+++ Client/src/app/services/signal-r.service.ts
@@ -0,0 +1,97 @@
+import { Injectable } from '@angular/core';
+import * as signalR from '@aspnet/signalr';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class SignalRService {
+
+  private connections;
+  private connectionsPromises;
+  private events;
+
+  constructor() {
+    this.connections = {};
+    this.connectionsPromises = {};
+    this.events = [];
+   }
+
+  createConnection(key, hubName, errorsHandler) {
+
+    if (this.connections[key]) {
+      return Promise.resolve();
+    }
+
+    this.connections[key] = new signalR.HubConnectionBuilder();
+
+
+    this.connections[key] = this.connections[key].withUrl(
+      `http://localhost:8000/${hubName}`);
+
+    this.connections[key] = this.connections[key]
+      .configureLogging(signalR.LogLevel.Information)
+      .build();
+
+
+    this.connectionsPromises[key] = this.connections[key]
+      .start()
+      .catch(err => {
+        console.log(`Could not create connection to ${hubName} hub: ${err}`);
+        throw err;
+      });
+
+    return this.connectionsPromises[key];
+  }
+
+
+  registerHandler(hubKey, functionName, handler) {
+
+    for (let i = 0; i < this.events.length; i++) {
+      if (this.events[i].hubKey === hubKey && this.events[i].functionName === functionName) {
+        return;
+      }
+    }
+
+    this.connections[hubKey].on(functionName, handler);
+    this.events.push({ hubKey, functionName });
+  }
+
+  unregisterHandlers() {
+    this.events.forEach(event => {
+      this.connections[event.hubKey].off(event.functionName);
+    });
+
+    this.events = null;
+  }
+
+  sendRequest(hubKey, serverMethodName, ...params) {
+    if (!this.connections || !this.connections[hubKey]) {
+      throw new Error('Connection not yet established');
+    }
+
+    params = params.filter((value) => value !== undefined);
+    return new Promise((resolve, reject) => {
+      this.connectionsPromises[hubKey]
+        .then(() => {
+          let p;
+
+          p = this.connections[hubKey]
+            .invoke(serverMethodName, ...params)
+
+          p.then(() => resolve())
+            .catch(err => {
+              console.log(`Could not send a request: ${err}, server method name: ${serverMethodName}`);
+              reject(err);
+            });
+        })
+        .catch((err) => reject(err));
+    });
+  }
+
+  isConnectionReady(hubKey) {
+    return (
+      this.connections[hubKey].state === signalR.HubConnectionState.Connected
+    );
+  }
+}
