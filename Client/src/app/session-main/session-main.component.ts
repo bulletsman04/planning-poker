493c8e019c3894a05b5b86cb429d6986b338d5a2
mwielgosz04 <mwielgosz04@gmail.com>
2020-04-09 23:37:27 +0200

    session screen - basic setup + choosing vote

--- Client/src/app/session-main/session-main.component.ts
+++ Client/src/app/session-main/session-main.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SignalRService } from '../services/signal-r.service';
+import { User } from '../models/user';
+import { SessionService } from '../services/session.service';
 
 @Component({
   selector: 'app-session-main',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SessionMainComponent implements OnInit {
 
-  constructor() { }
+  public voteOptions: number[];
 
-  ngOnInit(): void {
+  constructor(private signalR: SignalRService, public session: SessionService) {
+    this.voteOptions = [0, 0.5, 1];
+    let nextValue = 2;
+    let length;
+    while(nextValue < 40){
+      this.voteOptions.push(nextValue);
+      length = this.voteOptions.length;
+      nextValue = this.voteOptions[length - 2] + this.voteOptions[length - 1];
+    }
+
   }
 
+  ngOnInit(): void {
+    this.signalR.createConnection('manage-session', 'manage-session');
+    this.signalR.registerHandler('manage-session', 'UserJoined', this.userJoinedSessionHandler.bind(this));
+  }
+
+  userJoinedSessionHandler(users: User[]) {
+    this.session.users = users;
+  }
+
+  public vote(value: number){
+    this.session.vote(value);
+  }
 }
